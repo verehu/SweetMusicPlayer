9a5883c22770e1f104826f9aeb16ccca236bab42
caowei <646030315@qq.com>
2015-08-23 20:53:28 +0800

    test commit

--- app/src/main/java/com/huwei/sweetmusicplayer/ui/adapters/QueueAdapter.java
+++ app/src/main/java/com/huwei/sweetmusicplayer/ui/adapters/QueueAdapter.java
@@ -1,7 +1,7 @@
 package com.huwei.sweetmusicplayer.ui.adapters;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
+//import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
