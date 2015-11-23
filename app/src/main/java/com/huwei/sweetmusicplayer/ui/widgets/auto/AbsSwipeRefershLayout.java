014f4deadc1f70163a54335f8083780ff00f1224
Jerry <huweigoodboy@126.com>
2015-11-23 20:25:09 +0800

    加入上拉加载空间

---
+++ app/src/main/java/com/huwei/sweetmusicplayer/ui/widgets/auto/AbsSwipeRefershLayout.java
@@ -0,0 +1,30 @@
+package com.huwei.sweetmusicplayer.ui.widgets.auto;
+
+import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.AttributeSet;
+
+/**
+ * 为了屏蔽一些方法
+ * @author jerry
+ * @date 2015-11-23
+ */
+public abstract class AbsSwipeRefershLayout extends SwipeRefreshLayout {
+    public AbsSwipeRefershLayout(Context context) {
+        super(context);
+    }
+
+    public AbsSwipeRefershLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public final void setOnRefreshListener(OnRefreshListener listener) {
+        super.setOnRefreshListener(listener);
+    }
+
+    @Override
+    public final void setRefreshing(boolean refreshing) {
+        super.setRefreshing(refreshing);
+    }
+}
--- app/src/main/java/com/huwei/sweetmusicplayer/util/TimeUtil.java
+++ app/src/main/java/com/huwei/sweetmusicplayer/util/TimeUtil.java
@@ -3,7 +3,10 @@ package com.huwei.sweetmusicplayer.util;
 import android.R.integer;
 import android.util.Log;
 
- 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 
 public class TimeUtil {
 
@@ -58,5 +61,16 @@ public class TimeUtil {
 		
 		millTime=(min*60+second)*1000+mill*10;
 		return millTime;
+	}
+
+	public static String getCurrentTime(String format) {
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.getDefault());
+		String currentTime = sdf.format(date);
+		return currentTime;
+	}
+
+	public static String getCurrentTime() {
+		return getCurrentTime("yyyy-MM-dd  HH:mm:ss");
 	}
 }
